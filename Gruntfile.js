1c7b34c0fcc630b61ed773859bf5523b55a51c0e
Archie Lu <chienius@outlook.com>
2015-02-05 17:15:37 +0800

    Initial works 1. placed new static files in ./src/public dir 2. respectively modified Gruntfile 3. added a prototype of frontend design concept

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
         stylus: {
             devCompile: {
                 files: {
-                    './preview/css/style.styl': './src/view/css/style.styl'
+                    './preview/css/style.css': './src/view/css/style.styl'
                 }
             }
         },
@@ -21,11 +21,29 @@ module.exports = function(grunt) {
                 files: [
                     {
                         src: './node_modules/jquery/dist/jquery.min.js', 
-                        dest: './preview/js/'
+                        dest: './preview/js/jquery.min.js'
                     },
                     {
                         src: './node_modules/angular/angular.min.js',
-                        dest: './preview/js/'
+                        dest: './preview/js/angular.min.js'
+                    },
+                    {
+                        expand: true,
+                        cwd: './node_modules/font-awesome/',
+                        src: 'css/**',
+                        dest: './preview'
+                    },
+                    {
+                        expand: true,
+                        cwd: './node_modules/font-awesome/',
+                        src: 'fonts/**',
+                        dest: './preview'
+                    },
+                    {
+                        expand: true,
+                        cwd: './src/public',
+                        src: '**',
+                        dest: './preview'
                     }
                 ]
             }
