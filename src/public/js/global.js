74b68663095a4d59d2fb11f256ebb1a1eb660bda
Archie Lu <chienius@outlook.com>
2015-02-07 15:06:22 +0800

    Refine on user experience.

--- src/public/js/global.js
+++ src/public/js/global.js
@@ -10,4 +10,19 @@ app.controller("TrackListController", ["$scope", "$http", function($scope, $http
         });
 
     $scope.curState = curState;
+    $scope.getImgClass = function(){
+        if( window.innerHeight > window.innerWidth )
+            return 1;
+        else
+            return 0; 
+    };
 }]);
+
+app.filter("getBg", function(){
+    return function(trackId){
+        if(trackId > 0)
+            return "images/bgs/" + trackId.toString() + ".jpg";
+        else
+            return $('.bg-hover img').attr('src');
+    };
+});
